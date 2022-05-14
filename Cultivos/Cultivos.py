08c65e9316e7fdb1e4888e0ffb24c084c765b3ed
Rsilgal <rsilgal@gmail.com>
2022-05-14 20:39:26 +0200

    Renombrar las funciones de la clase Cultivos para que cumplan el estandar

--- Cultivos/Cultivos.py
+++ Cultivos/Cultivos.py
@@ -17,5 +17,5 @@ class Cultivos():
     def _str__(self): #Impresión que se mostrará a imprimir la clase
         return "El cultivo con el ID {0} tiene un area de {2} hecatreas, se puede cultivar en parcelas de los tipos {1}. Su duración es de {3} días.".format(self.identificador, self.posibleSuelo, self.areaMinima, self.duracionCultivo)
         
-    def diasTranscurridos(self, factorDecremento = 1): #Método mediante el cual restaremos la cantidad de días deseada a los cultivos. Por defecto, el decremento de días será 1.
+    def dias_transcurridos(self, factorDecremento = 1): #Método mediante el cual restaremos la cantidad de días deseada a los cultivos. Por defecto, el decremento de días será 1.
         self.duracionCultivo -= factorDecremento
